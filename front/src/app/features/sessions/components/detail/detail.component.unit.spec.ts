44f74c911d60ae8f9f25705a2bcebcf2f76f4c8c
tipikae <gilles.bd@free.fr>
2024-01-01 09:17:20 +0100

    Tests implemented

--- front/src/app/features/sessions/components/detail/detail.component.unit.spec.ts
+++ front/src/app/features/sessions/components/detail/detail.component.unit.spec.ts
@@ -7,6 +7,8 @@ import { expect } from '@jest/globals';
 import { SessionService } from '../../../../services/session.service';
 
 import { DetailComponent } from './detail.component';
+import { SessionApiService } from '../../services/session-api.service';
+import { of } from 'rxjs';
 
 
 describe('DetailComponent', () => {
@@ -21,6 +23,13 @@ describe('DetailComponent', () => {
     }
   }
 
+  const mockSessionApiService = {
+    detail: jest.fn().mockReturnValue(of()),
+    delete: jest.fn().mockReturnValue(of()),
+    participate: jest.fn().mockReturnValue(of()),
+    unParticipate: jest.fn().mockReturnValue(of())
+  }
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
@@ -31,18 +40,41 @@ describe('DetailComponent', () => {
       ],
       declarations: [DetailComponent], 
       providers: [
-        { provide: SessionService, useValue: mockSessionService }
+        { provide: SessionService, useValue: mockSessionService },
+        { provide: SessionApiService, useValue: mockSessionApiService }
       ],
     })
       .compileComponents();
       service = TestBed.inject(SessionService);
     fixture = TestBed.createComponent(DetailComponent);
     component = fixture.componentInstance;
+    component.sessionId = '1';
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it(`should call 'window.history.back()' when 'back()' is called`, () => {
+    jest.spyOn(window.history, 'back');
+    component.back();
+    expect(window.history.back).toHaveBeenCalled();
+  });
+
+  it(`should call 'sessionApiService.delete()' when 'delete()' is called`, () => {
+    component.delete();
+    expect(mockSessionApiService.delete).toHaveBeenCalled();
+  });
+
+  it(`should call 'sessionApiService.participate()' when 'participate()' is called`, () => {
+    component.participate();
+    expect(mockSessionApiService.participate).toHaveBeenCalled();
+  });
+
+  it(`should call 'sessionApiService.unParticipate()' when 'unParticipate()' is called`, () => {
+    component.unParticipate();
+    expect(mockSessionApiService.unParticipate).toHaveBeenCalled();
   });
 });
 
