504ba6e9a97cc7f813ec2e8335df05f5ba0ea9f7
tipikae <gilles.bd@free.fr>
2024-01-01 07:05:15 +0100

    Login unit tests implemented

--- front/src/app/features/auth/components/login/login.component.unit.spec.ts
+++ front/src/app/features/auth/components/login/login.component.unit.spec.ts
@@ -11,15 +11,24 @@ import { expect } from '@jest/globals';
 import { SessionService } from 'src/app/services/session.service';
 
 import { LoginComponent } from './login.component';
+import { AuthService } from '../../services/auth.service';
+import { of } from 'rxjs';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
 
+  const mockAuthService = {
+    login: jest.fn().mockReturnValue(of())
+  }
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [LoginComponent],
-      providers: [SessionService],
+      providers: [
+        SessionService,
+        { provide: AuthService, useValue: mockAuthService }
+      ],
       imports: [
         RouterTestingModule,
         BrowserAnimationsModule,
@@ -31,6 +40,7 @@ describe('LoginComponent', () => {
         ReactiveFormsModule]
     })
       .compileComponents();
+
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -38,5 +48,33 @@ describe('LoginComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it(`form should be invalid when field is missing or incorrect`, () => {
+    // email missing
+    component.form.controls.email.setValue(null);
+    component.form.controls.password.setValue('123456');
+    expect(component.form.valid).toBeFalsy();
+
+    // email incorrect
+    component.form.controls.email.setValue('test.com');
+    component.form.controls.password.setValue('123456');
+    expect(component.form.valid).toBeFalsy();
+
+    // password missing
+    component.form.controls.email.setValue('test@test.com');
+    component.form.controls.password.setValue(null);
+    expect(component.form.valid).toBeFalsy();
+  });
+
+  it(`form should be valid when fields are correct`, () => {
+    component.form.controls.email.setValue('test@test.com');
+    component.form.controls.password.setValue('123456');
+    expect(component.form.valid).toBeTruthy();
+  });
+
+  it(`should call 'authService.login()' when 'submit()' is called`, () => {
+    component.submit();
+    expect(mockAuthService.login).toHaveBeenCalled();
   });
 });
