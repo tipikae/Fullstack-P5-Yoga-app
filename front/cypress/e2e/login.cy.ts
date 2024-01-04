c15e18da04f3552107cb3360db23227d5fc4c202
tipikae <gilles.bd@free.fr>
2024-01-04 10:52:10 +0100

    Failed case added

--- front/cypress/e2e/login.cy.ts
+++ front/cypress/e2e/login.cy.ts
@@ -24,4 +24,17 @@ describe('Login spec', () => {
 
     cy.url().should('include', '/sessions')
   })
+
+  it('Login failed', () => {
+    cy.visit('/login')
+
+    cy.intercept('POST', '/api/auth/login', {
+      statusCode: 401
+    })
+
+    cy.get('input[formControlName=email]').type("yoga@studio.com")
+    cy.get('input[formControlName=password]').type(`${"test!1234"}{enter}{enter}`)
+
+    cy.contains('An error occurred')
+  })
 });
