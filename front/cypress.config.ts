15b71cc41bbf24cfa71718d8111a44499c611be7
tipikae <gilles.bd@free.fr>
2024-01-07 16:12:13 +0100

    Cypress config added

--- front/cypress.config.ts
+++ front/cypress.config.ts
@@ -6,11 +6,9 @@ export default defineConfig({
   fixturesFolder: 'cypress/fixtures',
   video: false,
   e2e: {
-    // We've imported your old cypress plugins here.
-    // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
       return require('./cypress/plugins/index.ts').default(on, config)
     },
-    baseUrl: 'http://localhost:4200',
+    baseUrl: 'http://localhost:4200'
   },
 })
